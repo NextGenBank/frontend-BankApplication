23e0820295f6d3d98a4a58decadb28386ad0ddb0
LucaDinu <114168899+LucaDinu@users.noreply.github.com>
2025-04-28 17:51:28 +0300

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CreateUserAccount from '../components/CreateUserAccount.vue'
-import Transfer from '../components/Transfer.vue'
-import Transactions from '../components/Transactions.vue'
+
+import CreateUserAccount from '@/components/CreateUserAccount.vue'
+import Transfer from '@/components/Transfer.vue'
+import Transactions from '@/components/Transactions.vue'
 
 const routes = [
   { path: '/', name: 'CreateAccount', component: CreateUserAccount },
   { path: '/transfer', name: 'Transfer', component: Transfer },
-  { path: '/transactions', name: 'Transactions', component: Transactions },
+  { path: '/transactions', name: 'Transactions', component: Transactions }
 ]
 
 const router = createRouter({
